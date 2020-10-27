60e63c34be4257439dd5937140e9f95adebe13b4
Josh Mann <jlmann72@gmail.com>
2020-10-27 19:56:43 -0400

    Fixed main function problem.
    Variables are now printing correctly

--- variables.py
+++ variables.py
@@ -1,2 +1,16 @@
 # Variables.py
 # Recreating variables C# assignment in python
+from datetime import date
+
+
+def main():
+    numberOfCupsOfCoffee = "3"
+    print("cups of coffee: " + numberOfCupsOfCoffee)
+    fullName = "Josh Mann"
+    print(fullName)
+    today = date.today()
+    print(today)
+
+
+if __name__ == "__main__":
+    main()
